a4584526b56cb2c53c5fe4e24079a0619527fbf0
pubdreamcc <493310907@qq.com>
2019-03-27 20:53:57 +0800

    three commit

--- lbxy-v1-vue/src/components/carousel.vue
+++ lbxy-v1-vue/src/components/carousel.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="carousel">
     <el-carousel :interval="3000" arrow="always" height="378px" trigger='click'>
+      <el-dropdown :hide-on-click="false" trigger="click" @visible-change="openNav">
+        <span class="el-dropdown-link"><img src="../assets/img/navigation.png" class="openNav"></span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item v-for="(nav, index) in navs" :key="index" @click.native="clearStyle"><span>{{nav}}</span><img src="../assets/img/icon@2x.png"></el-dropdown-item>
+          </el-dropdown-menu>
+      </el-dropdown>
       <el-carousel-item v-for="item in 6" :key="item">
       </el-carousel-item>
     </el-carousel>
+    <img src="../assets/img/close.png" class="closeNav" :style="{display: flag}">
   </div>
 </template>
 
@@ -11,9 +18,30 @@
 export default {
   name: '',
   data () {
-    return {}
+    return {
+      navs: ['立德树人', '安防学院', '农学院', '商学院', '机器学院', '电器学院', '幸福学院', '职场加油站', '建筑学院', '其他学院'],
+      flag: 'none'
+    }
   },
-  components: {}
+  components: {},
+  methods: {
+    openNav (flag) {
+      if (flag) {
+        this.flag = 'block'
+      } else {
+        this.flag = 'none'
+      }
+    },
+    clearStyle (ev) {
+      if (ev.target.parentNode.className === 'el-dropdown-menu__item') {
+        ev.target.parentNode.style.background = '#333333'
+        ev.target.parentNode.style.color = '#999999'
+      } else {
+        ev.target.style.background = '#333333'
+        ev.target.parentNode.style.color = '#999999'
+      }
+    }
+  }
 }
 </script>
 
@@ -47,8 +75,73 @@ export default {
     bottom: 15px;
     left: auto;
   }
+  .closeNav{
+    position: absolute;
+    bottom: 277px;
+    left: 16.4%;
+    background: #333333;
+    cursor: pointer;
+  }
   .el-carousel__arrow{
     background: rgba(255, 255, 255, 0);
     font-size: 34px;
+  }
+  .el-dropdown{
+    position: absolute;
+    left: 45px;
+    z-index: 3;
+  }
+  .el-dropdown .openNav{
+    cursor: pointer;
+  }
+  .el-dropdown-menu{
+    width: 208px;
+    height: 378px;
+    background: #333333;
+    border-top-left-radius: 10px;
+    border: none;
+    margin: 0;
+    box-shadow: none;
+    padding: 0;
+    top: 51px !important;
+    left: 163px !important;
+    border-bottom-left-radius: 10px;
+    overflow: hidden;
+  }
+  .el-dropdown-menu__item{
+    position: relative;
+    width: 208px;
+    height: 37.8px;
+    color: #999999;
+    padding: 0;
+  }
+  .el-dropdown-menu__item:hover{
+    background: #9B9B9B !important;
+    color: white !important;
+  }
+  .el-dropdown-menu__item:hover img{
+    border: 1px dashed white;
+    background: rgba(255, 255, 255, .5);
+  }
+  .el-dropdown-menu__item span{
+    position: absolute;
+    vertical-align: middle;
+    font-size: 13px;
+    line-height: 37.8px;
+    width: 117px;
+    left: 50px;
+    overflow: hidden;
+  }
+  .el-dropdown-menu__item img{
+    position: absolute;
+    height: 17px;
+    width: 17px;
+    right: 68px;
+    vertical-align: middle;
+    top: 10px;
+    font-size: 28px;
+  }
+  .popper__arrow{
+    display: none !important;
   }
 </style>
